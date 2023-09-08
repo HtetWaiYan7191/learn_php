36231e1b47511689ae424924266ca955142424e1
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-08 17:04:10 +0630

    Add error_handling.php

---
+++ error_handling.php
@@ -0,0 +1,10 @@
+<?php
+
+//basic ways to handle errors 
+/* 
+E_PARSE syntax eror
+E_ERROR stop program
+E_WARNING warning but still run
+E_NOTICE likely to happen error
+E_STRICT type strict
+E_DEPERCATED should use modern code . 
