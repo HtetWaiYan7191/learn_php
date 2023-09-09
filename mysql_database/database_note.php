c9ec9eea3b47f452e0f0f6535eebb62812ff8d86
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-09 15:19:05 +0630

    Add laravel project

---
+++ mysql_database/database_note.php
@@ -0,0 +1,16 @@
+<?php
+
+/* five types of index 
+
+index, unique, primary => common use
+fulltext, spatial => rare use 
+
+In each table there is only one primary index 
+but there can be more than one unique index 
+
+if you want to set some thing as primary index then you should set that to id in database 
+
+//how to connect data base with php
+// we will use PDO class 
+// what is PDO class ? PDO => extension for php to connect it with database
+when we install xampp PDO is already in it . 
