c5416257ac7999aabfd523102d4c7f7488ed530d
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-10 12:19:25 +0630

    web.php file is a route file for all url for the project

--- laravel_project_1/blog/routes/web.php
+++ laravel_project_1/blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,26 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// Route is basic method which comes from frame work it needs two parameters 
+//1 path and
+//2 function 
 
-Route::get('/', function () {
-    return view('welcome');
+// what are routes //what is static route and what is dynamic route
+// static routes
+Route::get('/', [ArticleController::class, 'index']);
+Route::get('/articles', [ArticleController::class, 'index']);
+
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
+
+//dynamic routes go the the page depending on the para
+// Route::get('/articles/detail/{id}', function ($id) {
+//     return "Article Detail - $id";
+// });
+
+
+//routes can set route name
+//route pattern -> resource/action/id/sub-resource/sub-action action => add/update/view/delete
+Route::get('/users/detail/{id}/photos', function($id) {
+    return "Users Detail - $id";
 });
+
